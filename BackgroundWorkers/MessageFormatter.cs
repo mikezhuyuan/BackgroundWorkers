4a49e9d475b6f9a96a042df594e7c9456cee6fb1
Buddhike de Silva <buddhike.desilva@geeksdiary.com>
2014-01-16 11:19:13 +1100

    Message formatter interface support serializing without type information. We need this to build the log.

--- BackgroundWorkers/MessageFormatter.cs
+++ BackgroundWorkers/MessageFormatter.cs
@@ -6,15 +6,15 @@ namespace BackgroundWorkers
 {
     public interface IMessageFormatter
     {
-        string Serialize(object @object);
-        object Deserialize(string item);
+        string Serialize(object @object, bool preserveTypeInformation = true);
+        object Deserialize(string item, bool preserveTypeInformation = true);
     }
 
     public class MessageFormatter : IMessageFormatter
     {
-        public string Serialize(object @object)
+        public string Serialize(object @object, bool preserveTypeInformation = true)
         {
-            var s = BuildSerializer(); 
+            var s = BuildSerializer(preserveTypeInformation); 
 
             var sb = new StringBuilder();
             var writer = new StringWriter(sb);
@@ -24,18 +24,18 @@ namespace BackgroundWorkers
             return sb.ToString();
         }
 
-        public object Deserialize(string item)
+        public object Deserialize(string item, bool preserveTypeInformation = true)
         {            
-            return BuildSerializer().Deserialize(new JsonTextReader(new StringReader(item)));
+            return BuildSerializer(preserveTypeInformation).Deserialize(new JsonTextReader(new StringReader(item)));
         }
 
-        static JsonSerializer BuildSerializer()
+        static JsonSerializer BuildSerializer(bool preserveTypeInformation)
         {
             // It's important to preserve CLR type in the serialized payload
             // because it helps us to resolve the correct message handler to 
             // process the message. See WorkItemDispatcher.Process method for
             // more information.
-            return new JsonSerializer { TypeNameHandling = TypeNameHandling.All };
+            return new JsonSerializer { TypeNameHandling =  preserveTypeInformation ? TypeNameHandling.All : TypeNameHandling.None };
         }
     }
 }
